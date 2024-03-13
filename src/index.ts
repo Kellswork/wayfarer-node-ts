e0bd69d5c21b64eb507d617829c17dda88a2fd8c
Kellswork <kellogbonna@gmail.com>
2024-03-13 12:02:52 +0100

    add migration folders and migrate script

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,52 @@
-import express, { Application, Request, Response} from 'express';
-import dotenv from 'dotenv'
-import { debug } from 'console';
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+import { connectDB } from "./config/db";
 
 export interface RootResponse {
   message: string;
 }
 
-dotenv.config();
-
-const app:Application = express();
-const PORT = process.env.PORT;
+const app: Application = express();
+const PORT = config.PORT;
 
 app.get("/", (req: Request, res: Response<RootResponse>) => {
   res.status(200).json({
-    message : 'Welcome to wayfarer API'
-  })
-})
+    message: "Welcome to wayfarer API",
+  });
+});
+const dbUrl = config.dbUrl ?? "";
+// does this close every pool instance created?
+const { pool } = connectDB( dbUrl);
 
 export const server = app.listen(PORT, () => {
- console.log('Server started on port: ', PORT)
-})
+  console.log("Server started on port: ", PORT);
+});
 
-process.on('SIGTERM', () => {
-  console.info('SIGTERM signal recived: Shuting down server')
+process.on("SIGTERM", () => {
+  console.info("SIGTERM signal recived: Shuting down server");
+
   // perfomr clean up task here
-  server.close(()=> {
-    console.info('server is shutdown')
-    process.exit(0)
-  })
-  
-})
-process.on('SIGINT', () => {
-  debug('SIGINT signal received: Shuting down server');
-   // perfomr clean up task here
-  server.close(()=> {
-    console.info('server is shutdown')
-    process.exit(0)
-  })
+  pool.end(() => {
+    console.info("Database Pool has been closed");
+  });
+
+  server.close(() => {
+    console.info("server is shutdown");
+    process.exit(0);
+  });
+});
+
+process.on("SIGINT", () => {
+  console.info("SIGINT signal received: Shuting down server");
+
+  // perfomr clean up task here
+  pool.end(() => {
+    console.info("Database Pool has been shut down");
+  });
+
+  // perfomr clean up task here
+  server.close(() => {
+    console.info("server is shutdown");
+    process.exit(0);
+  });
 });
