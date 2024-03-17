1d0feabefd3f14d1cb379d602650f6b5560a0d86
Kellswork <kellogbonna@gmail.com>
2024-03-17 17:26:41 +0100

    jwt token generator. I wanted to see if I could use private keys ESCDA instead like in Golang

---
+++ src/helpers/jwtTokenHelper.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+require('dotenv').config();
+
+
+console.log('jwt', process.env.JWT_SECRET)
+export const generateToken = (payload: {id: string, isAdmin: boolean}) => {
+  return jwt.sign(
+    {
+      id: payload.id,
+      isAdmin: payload.isAdmin,
+    },
+    process.env.JWT_SECRET as string,
+    { expiresIn: '7d' },
+  );
+};
+
+export const verifyToken = (token: string) => {
+  if (!token) return false;
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string);
+    return decodedToken;
+  } catch (error) {
+    return false;
+  }
+};
