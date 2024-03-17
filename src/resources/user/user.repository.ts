5c0e4cd01e0620dc3c18c6bf6bf19e0cd0a7102d
Kellswork <kellogbonna@gmail.com>
2024-03-17 02:52:37 +0100

    repo for db logic

--- src/resources/user/user.repository.ts
+++ src/resources/user/user.repository.ts
@@ -1,40 +1,40 @@
-import { QueryResult } from "pg";
+import { Pool } from "pg";
 import * as models from "../../models/users";
+// import { PgError } from "../../models/DatabaseError";
 
 export interface UserRepository {
-createUser: CreateUser
+  createUser: CreateUser;
 }
 
 interface CreateUser {
   user: models.User;
-  db: {
-    query: (
-      text: string,
-      params?: [string, string, string, string, string, boolean, string, string]
-    ) => Promise<QueryResult>;
-  };
+  db: Pool;
 }
 
-export const createUser = async ({ user, db }: CreateUser): Promise<QueryResult<models.User> | undefined> => {
-  try {
-    const query = "INSERT INTO users (id, first_name, last_name, email, password, is_admin, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) returning id, isAdmin, created_at";
+export const createUser = async ({ db, user }: CreateUser) => {
+  const query =
+    "INSERT INTO users (id, first_name, last_name, email, password, is_admin, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) returning id, isAdmin, created_at";
 
-   const result = await db.query(query, [
-      user.id,
-      user.firstname,
-      user.lastname,
-      user.email,
-      user.password,
-      user.isAdmin,
-      user.createdAt,
-      user.updatedAt,
-    ]);
-    console.log('create-user:', result)
-    return result;
-  } catch (error) {
-    // TODO: edit this part later to return the correct error message from the postgres
-    console.log("an err occured:", error);
-  }
-}
+  const result = await db.query(query, [
+    user.id,
+    user.firstname,
+    user.lastname,
+    user.email,
+    user.password,
+    user.isAdmin,
+    user.createdAt,
+    user.updatedAt,
+  ]);
+  console.log("create-user:", result);
+  return result;
+};
+// catch (error: unknown) {
+//   // TODO: edit this part later to return the correct error message from the postgres
+//   const errorDetails = error as PgError
+//   const errorMesasge =`create user error:  ${errorDetails.code}-${errorDetails.message}. ${errorDetails.hint}`;
+//   console.error(errorMesasge)
+//   return errorMesasge;
+// }
 
-// validate email address
+
+// use a trycatch here to try an return an error here, I want the controller to be neater.
