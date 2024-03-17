54d08883bfb2f45b40a46e3adaf9e10c9e4794e1
Kellswork <kellogbonna@gmail.com>
2024-03-17 02:38:57 +0100

    i spent soo much time debuging this error: Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client and it was because next() was calle twice in the dbmiddleware

--- src/middlewares/dbMiddleWare.ts
+++ src/middlewares/dbMiddleWare.ts
@@ -1,18 +1,19 @@
- // Assuming you're using pg
-import {NextFunction, Request, Response } from 'express'
+// Assuming you're using pg
+import { NextFunction, Request, Response } from "express";
 import { connectDB } from "../config/db";
-import config from '../config';
+import config from "../config";
 
-export const dbMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const dbMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const dbUrl = config.dbUrl ?? "";
     req.app.locals.db = connectDB(dbUrl); // Inject the connection pool into req object
     next();
   } catch (error) {
-    console.error('Error connecting to database:', error);
-    return res.status(500).json({
-      error: "could not connet to database",
-  })
+    console.error("Error connecting to database:", error);
+    return;
+  }
 };
-next();
-}
