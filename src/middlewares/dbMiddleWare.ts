e09233479abde34e908f95979d69d3eb12ac9579
Kellswork <kellogbonna@gmail.com>
2024-03-20 19:25:53 +0100

    consolde.log connection info

--- src/middlewares/dbMiddleWare.ts
+++ src/middlewares/dbMiddleWare.ts
@@ -11,6 +11,9 @@ export const dbMiddleware = (
   try {
     const dbUrl = config.dbUrl ?? "";
     req.app.locals.db = connectDB(dbUrl); // Inject the connection pool into req object
+    console.log("Total connections:", req.app.locals.db.totalCount);
+    console.log("Idle connections:", req.app.locals.db.idleCount);
+    console.log("Waiting clients:", req.app.locals.db.waitingCount);
     next();
   } catch (error) {
     console.error("Error connecting to database:", error);
